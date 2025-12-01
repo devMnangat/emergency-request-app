f2e945ca4141018461db4ebae7a2f419674be3d4
Emmanuel Mnangat <emnangat99@gmail.com>
2025-12-01 12:36:33 +0300

    add mongodm atlas connection

--- src/mongoose/dbConnect.ts
+++ src/mongoose/dbConnect.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 export async function dbConnect() {
     try {
-        await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI as string);
+        await mongoose.connect(process.env.MONGODB_URI as string);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
